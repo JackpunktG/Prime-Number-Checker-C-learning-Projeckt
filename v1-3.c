5cd0a8e085bbf0b8a1ca8010e45dd1aa49dba04e
Jack <jack.goldsbrough@outlook.com>
2025-03-21 09:22:31 +0100

    V1-3
    Made the calculation into a callable function. 
    Added a loop to not close the program after each check

---
+++ v1-3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <stdbool.h>
+
+
+
+bool is_prime(int prime)
+{
+	int multip1;
+	int multip2;
+	int result1;
+	
+	multip1 = 1;
+	multip2 = 1;
+	
+	while (multip2 <= (prime / 2)) {   			//is_prime = prime funktion
+		while (multip1 < prime) {
+			result1 = (multip1 * multip2);		// Einmaleins
+			multip1++;
+			printf("%d\n", result1);
+			if (result1 == prime) { 				//endet sofort wert = prime
+				return false;
+			}
+		}		
+		multip2++;				// erhöhert die Einmaleins bis alle ausgeführt sind
+		multip1 = 1;			// zurücksetzen multiple
+	}
+	return true;
+}
+
+int main () 
+{
+	
+	int prime; 
+
+	while (1) {					
+		printf("\nTo exit programm enter -1\n\nPick your number wisely...\n");
+		scanf("%d", &prime);	// wo du nummer eingibst, dass du testen möchtest
+		if (prime == -1)		// end programm Möglichkeit
+			return 0;
+		
+		clock_t start, end;
+		start = clock();   //  Zeitmessung start
+		
+		if (is_prime(prime) == true) {  	 //ruft is_prime funktion
+			printf("\nPRIME!!\n");
+		}
+		else { 	
+			printf("\nNo Dice... better luck nexttt time ;)\n");
+		}
+		end = clock(); 																	 // Zeitmessung ende
+		printf("Execution time: %.6f seconds\n", (double)(end - start) / CLOCKS_PER_SEC); //Zeit Berechnung
+	}
+    return 0;
+ }
+ 
